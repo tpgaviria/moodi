ae4ebeee22eb791b2d0bbaa01c611b349eca165b
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-08-06 21:30:02 -0400

    deploy

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,14 @@ const path = require('path');
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+
 router.use('/api', apiRoutes);
 router.get('/auth', function (req, res) {
     console.log('hi')
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
 });
 
 module.exports = router;
