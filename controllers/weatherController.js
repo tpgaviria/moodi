52d0e3e1d2c9b5e790e4441578be93aa7276376d
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-08-08 19:07:25 -0400

    city updated

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 module.exports = {
     getWeather: function (req, res) {
-        const { query: params } = req;
+        const { id: params } = req;
         const KEY = process.env.WEATHER_KEY;
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?appid=${KEY}`, { params })
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?appid=${KEY}&id=4346455`)
             .then(results => {
                 res.json(results.data);
             })
